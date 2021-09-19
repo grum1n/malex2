29a5198ab41e233d2276a855d39c0509deede855
grum1n <grumin.el.pastas@gmail.com>
2021-09-19 15:57:16 +0300

    added js menu button script

---
+++ app/javascript/custom.js
@@ -0,0 +1,7 @@
+/*menu button*/
+function openNav() {
+    document.getElementById("menu").style.width = "100%";
+  }
+  function closeNav() {
+    document.getElementById("menu").style.width = "0";
+}
