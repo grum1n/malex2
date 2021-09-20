fc9043ac1fe68beddb026a81e38206eff1694984
grum1n <grumin.el.pastas@gmail.com>
2021-09-20 08:18:56 +0300

    changed index.php file

---
+++ app/views/footer.php
@@ -0,0 +1,11 @@
+<footer class="row bg-black">
+    <div class="container flex-between">
+        <a href=""><img src="app/images/Logo.png" alt="Logo"></a>
+        <p>© 2020 Business Consulting Agency Figma Template By Adveits</p>
+        <div>
+            <a href=""><i class="fab fa-instagram"></i></a>
+            <a href=""><i class="fab fa-facebook-f"></i></a>
+        </div>
+    </div>
+</footer>
+<script src="app/javascript/custom.js"></script>     
