31c4c9c71342fdd6fd97b5e2ea69bd566e5917cd
grum1n <grumin.el.pastas@gmail.com>
2021-09-20 15:31:22 +0300

    added meet-our-team style

---
+++ app/views/include/meet-our-team.php
@@ -0,0 +1,53 @@
+<section class="row meet-our-team">
+    <div  class="container">
+        <ul>
+            <li class="container-big-text">
+                <h3 class="section-name">MEET OUR TEAM</h3>
+                <h2 class="section-description">We are Business Consulting Professionals</h2>
+                <span>Team</span>
+            </li>
+            <li class="flex-container">
+                <div >
+                    <div class="portret">
+                        <img src="app/images/pexels-djordje-petrovic-2102415.jpg" alt="Professional">
+                        <span class="text-poppins-700">CEO</span>
+                    </div>
+                    <h4>Tom Henders</h4>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.</p>
+                    <div class="icon-st flex-container">
+                        <a href=""><i class="fab fa-facebook-f"></i></a>
+                        <a href=""><i class="fab fa-linkedin-in"></i></a>
+                        <a href=""><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+                <div>
+                    <div class="portret">
+                        <img src="app/images/pexels-djordje-petrovic-2102415.jpg" alt="Professional">
+                        <span class="text-poppins-700">Business Analytic</span>
+                    </div>
+                    <h4>Andrew Mike</h4>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.</p>
+                    <div class="icon-st flex-container">
+                        <a href=""><i class="fab fa-facebook-f"></i></a>
+                        <a href=""><i class="fab fa-linkedin-in"></i></a>
+                        <a href=""><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+                <div>
+                    <div class="portret">
+                        <img src="app/images/pexels-djordje-petrovic-2102415.jpg" alt="Professional">
+                        <span class="text-poppins-700">Marketing Agent</span>
+                    </div>
+                    <h4>Rosie Ford</h4>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.</p>
+                    <div class="icon-st flex-container">
+                        <a href=""><i class="fab fa-facebook-f"></i></a>
+                        <a href=""><i class="fab fa-linkedin-in"></i></a>
+                        <a href=""><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        
+    </div>
+</section>
