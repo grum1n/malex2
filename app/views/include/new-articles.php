1ecd41a5a48d269fa14b096ff01a1d4f39921859
grum1n <grumin.el.pastas@gmail.com>
2021-09-21 15:26:37 +0300

    added new articles responsive style

--- app/views/include/new-articles.php
+++ app/views/include/new-articles.php
@@ -6,13 +6,28 @@
                 <h2 class="section-description">Check Our Latest Blog Posts</h2>
                 <span>News</span>
             </li>
-            <li>
+            <li class="flex-container mobile-new-articles">
                 <div>
-                    <img src="" alt="">
-                    <h5>5 Amazing and Easy Tips for Sustainable Business Success</h5>
+                    <img src="app/images/news1-pexels-markus-winkler.jpg" alt="">
+                    <h5 class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
                     <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
-                    <a href=""><img src="app/images/Button.png" alt=""><span>READ MORE</span></a>
+                    <a href=""><img src="app/images/Button.png" alt="">READ MORE</a>
                 </div>
+                <div>
+                    <img src="app/images/news2-pexels-kaboompics-com-6335.jpg" alt="">
+                    <h5  class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
+                    <a href=""><img src="app/images/Button.png" alt="">READ MORE</a>
+                </div>
+                <div>
+                    <img src="app/images/news1-pexels-markus-winkler.jpg" alt="">
+                    <h5  class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
+                    <a href=""><img src="app/images/Button.png" alt="">READ MORE</a>
+                </div>
+            </li>
+            <li>
+                <a href="">View All Posts</a>
             </li>
         </ul>
     </div>
