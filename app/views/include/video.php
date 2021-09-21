eba968ab57f3f3e13b320909140442c746349175
grum1n <grumin.el.pastas@gmail.com>
2021-09-21 12:44:30 +0300

    added video style

---
+++ app/views/include/video.php
@@ -0,0 +1,3 @@
+<section class="row video">
+    <iframe src="https://www.youtube.com/embed/I7YAXgT_WLQ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
+</section>
