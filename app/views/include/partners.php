9944541cce65c54df25ce736a2d299bd38ca3708
grum1n <grumin.el.pastas@gmail.com>
2021-09-21 12:19:51 +0300

    added partners style ,responsive

--- app/views/include/partners.php
+++ app/views/include/partners.php
@@ -1,10 +1,12 @@
-<section>
-    <ul>
-        <li><img src="app/images/partner1.png" alt="Partners"></li>
-        <li><img src="app/images/partner2.png" alt="Partners"></li>
-        <li><img src="app/images/partner3.png" alt="Partners"></li>
-        <li><img src="app/images/partner2.png" alt="Partners"></li>
-        <li><img src="app/images/partner3.png" alt="Partners"></li>
-        <li><img src="app/images/partner1.png" alt="Partners"></li>
-    </ul>
+<section class="row">
+   <div class="container partners">
+        <ul class="flex-container">
+                <li><img src="app/images/partner1.png" alt="Partners"></li>
+                <li><img src="app/images/partner2.png" alt="Partners"></li>
+                <li><img src="app/images/partner3.png" alt="Partners"></li>
+                <li><img src="app/images/partner2.png" alt="Partners"></li>
+                <li><img src="app/images/partner3.png" alt="Partners"></li>
+                <li><img src="app/images/partner1.png" alt="Partners"></li>
+        </ul>
+   </div>
 </section>
