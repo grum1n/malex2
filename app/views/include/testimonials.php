f1a80a4691ffc5e929e6c4a6da24cf2de2b012f3
grum1n <grumin.el.pastas@gmail.com>
2021-09-21 13:44:27 +0300

    added testimonials responsive

--- app/views/include/testimonials.php
+++ app/views/include/testimonials.php
@@ -1,5 +1,22 @@
-<section class="row">
+<section class="row testimonials">
     <div class="container">
-
+        <ul>
+            <li>
+                <h3 class="section-name">TESTIMONIALS</h3>
+                <h2 class="section-description">Our Happy Customers Reviews</h2>
+            </li>
+            <li class="text-poppins-400">
+            <i class="fas fa-quote-left"></i><span>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account.</span>
+            </li>
+            <li>
+                <ol class="flex-container">
+                    <li><i class="fas fa-circle"></i><i class="fas fa-circle"></i><i class="fas fa-circle"></i></li>
+                    <li> 
+                        <h4 class="text-poppins-700">Rosie Ford</h4>
+                        <p  class="text-poppins-400">CEO at Company</p>
+                    </li>
+                </ol>
+            </li>
+        </ul>
     </div>
 </section>
