d62728b9adb030c49733660d8c0586470ea7e3e0
grum1n <grumin.el.pastas@gmail.com>
2021-09-21 11:52:26 +0300

    added price plans style

---
+++ app/views/include/price-plans.php
@@ -0,0 +1,47 @@
+<section class="row price-plans">
+    <div  class="container">
+        <ul>
+            <li class="container-big-text">
+                <h3 class="section-name">PRICE PLANS</h3>
+                <h2 class="section-description">Choose the Best Plan for You</h2>
+                <span>Plans</span>
+            </li>
+            <li class="flex-container">
+                <ul  class="plans">
+                    <li><h4>Basic Plan</h4></li>
+                    <li>This is a Basic Plan</li>
+                    <li><h5>79<i class="fas fa-dollar-sign"></i><span>/ MONTHLY</span></h5></li>
+                    <li class="flex-between border-bottom">Branding <i class="fas fa-check"></i></li>
+                    <li class="flex-between border-bottom">20 hours of consultation <i class="fas fa-check"></i></li>
+                    <li class="flex-between border-bottom">15 hours if theory <i class="fas fa-check"></i></li>
+                    <li class="flex-between border-bottom">Skype video call <i class="fas fa-check"></i></li>
+                    <li class="flex-between">Support 24/7 <i class="fas fa-check"></i></li>
+                    <li class="flex-container"><a href="">Choose Plan</a></li>
+                </ul>
+                <ul class="plans">
+                    <li><h4>Standart Plan</h4></li>
+                    <li>This is a Basic Plan</li>
+                    <li><h5>129<i class="fas fa-dollar-sign"></i><span>/ MONTHLY</span></h5></li>
+                    <li class="flex-between border-bottom">Branding <i class="fas fa-check"></i></li>
+                    <li class="flex-between border-bottom">20 hours of consultation <i class="fas fa-check"></i></li>
+                    <li class="flex-between border-bottom">15 hours if theory <i class="fas fa-check"></i></li>
+                    <li class="flex-between border-bottom">Skype video call <i class="fas fa-check"></i></li>
+                    <li class="flex-between">Support 24/7 <i class="fas fa-check"></i></li>
+                    <li class="flex-container"><a href="">Choose Plan</a></li>
+                </ul>
+                <ul  class="plans">
+                    <li><h4>Standart Plan</h4></li>
+                    <li>This is a Basic Plan</li>
+                    <li><h5>259<i class="fas fa-dollar-sign"></i><span>/ MONTHLY</span></h5></li>
+                    <li class="flex-between border-bottom">Branding <i class="fas fa-check"></i></li>
+                    <li class="flex-between border-bottom">20 hours of consultation <i class="fas fa-check"></i></li>
+                    <li class="flex-between border-bottom">15 hours if theory <i class="fas fa-check"></i></li>
+                    <li class="flex-between border-bottom">Skype video call <i class="fas fa-check"></i></li>
+                    <li class="flex-between">Support 24/7 <i class="fas fa-check"></i></li>
+                    <li class="flex-container"><a href="">Choose Plan</a></li>
+                </ul>
+            </li>
+        </ul>
+        
+    </div>
+</section>
