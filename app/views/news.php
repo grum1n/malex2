bbeb1ca65281938178c5d773162bde4f2adcf38c
grum1n <grumin.el.pastas@gmail.com>
2021-09-22 12:25:52 +0300

    added news style to right boxes

--- app/views/news.php
+++ app/views/news.php
@@ -34,14 +34,51 @@
         </ol>
         <div class="col-2">
             <div>
-                <h5 class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
-                <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
+                <input type="text" value="" placholder="Search">
+                <button><i class="fas fa-search"></i></button>
+            </div>
+            <div>
+                <p>Categories</p>
+                <ul>
+                    <li>Business</li>
+                    <li>Marketing</li>
+                    <li>Sales</li>
+                    <li>Strategy</li>
+                </ul>
             </div>
         </div>
         <div class="col-3">
+            <ul>
+                <li class="text-poppins-700">Recent Posts</li>
+                <li><img src="app/images/money-mini.jpg" alt=""><span>5 Amazing and Easy Tips for Sustainable Business Success</span></li>
+                <li><img src="app/images/money-mini.jpg" alt=""><span>Assessing the Maturity of Your Data Management in Industry</span></li>
+                <li><img src="app/images/money-mini.jpg" alt=""><span>How to Build Your Sustainable Susiness Strategy</span></li>
+                <li><img src="app/images/money-mini.jpg" alt=""><span>5 Amazing and Easy Tips for Sustainable Business Success</span></li>
+            </ul>
+            <ol>
+                <li class="text-poppins-700">Recent Comments</li>
+                <li>
+                    <h3>Rosie Ford on</h3>
+                    <h4 class="text-poppins-400">Assessing the Maturity of Your Data Management in Industry</h4>
+                </li>
+                <li>
+                    <h3>Rosie Ford on</h3>
+                    <h4 class="text-poppins-400">Assessing the Maturity of Your Data Management in Industry</h4>
+                </li>
+                <li>
+                    <h3>Rosie Ford on</h3>
+                    <h4 class="text-poppins-400">Assessing the Maturity of Your Data Management in Industry</h4>
+                </li>
+            </ol>
             <div>
-                <h5 class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
-                <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
+                <h3>Tags</h3>
+                <ul>
+                    <li>Business</li>
+                    <li>Marketing</li>
+                    <li>Sales</li>
+                    <li>Strategy</li>
+                </ul>
+
             </div>
         </div>
     </div>
