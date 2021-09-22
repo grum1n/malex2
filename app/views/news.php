54ee3bfeb509a6d4f3d331e98e75c7a74d53f937
grum1n <grumin.el.pastas@gmail.com>
2021-09-22 09:26:21 +0300

    changed pages-top-section responsive style

--- app/views/news.php
+++ app/views/news.php
@@ -1,6 +1,9 @@
-<section class="row">
+<section class="row pages-top-section">
     <div class="container">
-  
+        <div class="container-big-text">
+            <h1>Our News</h1>
+            <span>News</span>
+        </div> 
     </div>
 </section>
 <?php
--- app/views/service.php
+++ app/views/service.php
@@ -1,5 +1,20 @@
+<section class="row pages-top-section">
+    <div class="container">
+        <div class="container-big-text">
+            <h1>Strategic Planning</h1>
+            <p>A business consulting agency is involved in the planning, implementation, and education of businesses. We work directly with business owners on developing a business plan, identifying marketing needs and developing the necessary skills for business ownership.</p>
+            <span>Planning</span>
+        </div> 
+    </div>
+    <div class="container">
+        <?php
+             include('include/video.php');
+        ?>
+
+    </div>
+</section>
 <?php
-include('include/video.php');
+
 include('include/how-it-works.php');
 include('include/price-plans.php');
 include('include/partners.php');
