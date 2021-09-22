9ce94233ff49893ddd467ebb90222dc7415efac1
grum1n <grumin.el.pastas@gmail.com>
2021-09-22 10:05:04 +0300

    adde news.php page

--- app/views/news.php
+++ app/views/news.php
@@ -5,7 +5,44 @@
             <span>News</span>
         </div> 
     </div>
+    <div class="container all-news-grid">
+        <ol class="col-1 flex-container">
+            <li>
+                <img src="app/images/news1-pexels-markus-winkler.jpg" alt="">
+                <h5 class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
+                <a href=""><img src="app/images/Button.png" alt="">READ MORE</a>
+            </li>
+            <li>
+                <img src="app/images/news2-pexels-kaboompics-com-6335.jpg" alt="">
+                <h5  class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
+                <a href=""><img src="app/images/Button.png" alt="">READ MORE</a>
+            </li>
+            <li>
+                <img src="app/images/news1-pexels-markus-winkler.jpg" alt="">
+                <h5 class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
+                <a href=""><img src="app/images/Button.png" alt="">READ MORE</a>
+            </li>
+            <li>
+                <img src="app/images/news2-pexels-kaboompics-com-6335.jpg" alt="">
+                <h5  class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
+                <a href=""><img src="app/images/Button.png" alt="">READ MORE</a>
+            </li>
+        </ol>
+        <div class="col-2">
+            <div>
+                <h5 class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
+            </div>
+        </div>
+        <div class="col-3">
+            <div>
+                <h5 class="text-poppins-700">5 Amazing and Easy Tips for Sustainable Business Success</h5>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducims…</p>
+            </div>
+        </div>
+    </div>
 </section>
-<?php
-include('include/new-articles.php');
-?>
